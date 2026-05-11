77beb98aeee2318c1a9aa4826b945a677a363860
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2026-05-11 14:50:55 +0600

    finished adding all off the game logics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import { FlatList, StatusBar, StyleSheet, Text, View } from 'react-native';
 import React, { JSX, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
@@ -24,15 +24,13 @@ export default function App(): JSX.Element {
       gameState[1] == gameState[2]
     ) {
       setGameWinner(`${gameState[0]} won the game! 🥳`);
-    }
-    if (
+    } else if (
       gameState[3] != 'empty' &&
       gameState[3] == gameState[4] &&
       gameState[4] == gameState[5]
     ) {
       setGameWinner(`${gameState[3]} won the game! 🥳`);
-    }
-    if (
+    } else if (
       gameState[6] != 'empty' &&
       gameState[6] == gameState[7] &&
       gameState[7] == gameState[8]
@@ -41,21 +39,19 @@ export default function App(): JSX.Element {
     }
 
     // top to bottom
-    if (
+    else if (
       gameState[0] != 'empty' &&
       gameState[0] == gameState[3] &&
       gameState[3] == gameState[6]
     ) {
       setGameWinner(`${gameState[0]} won the game! 🥳`);
-    }
-    if (
+    } else if (
       gameState[1] != 'empty' &&
       gameState[1] == gameState[4] &&
       gameState[4] == gameState[7]
     ) {
       setGameWinner(`${gameState[1]} won the game! 🥳`);
-    }
-    if (
+    } else if (
       gameState[2] != 'empty' &&
       gameState[2] == gameState[5] &&
       gameState[5] == gameState[8]
@@ -64,7 +60,7 @@ export default function App(): JSX.Element {
     }
 
     // cross access 0-8
-    if (
+    else if (
       gameState[0] != 'empty' &&
       gameState[0] == gameState[4] &&
       gameState[4] == gameState[8]
@@ -73,13 +69,38 @@ export default function App(): JSX.Element {
     }
 
     // cross access 2-6
-    if (
+    else if (
       gameState[2] != 'empty' &&
       gameState[2] == gameState[4] &&
       gameState[4] == gameState[6]
     ) {
       setGameWinner(`${gameState[2]} won the game! 🥳`);
+    } else if (!gameState.includes('empty', 0)) {
+      setGameWinner(`Game is Draw...!⌛`);
     }
+  };
+
+  const onChangeItem = (itemNumber: number) => {
+    if (gameWinner) {
+      return Snackbar.show({
+        text: gameWinner,
+        backgroundColor: '#000000',
+        textColor: '#ffffff',
+      });
+    }
+
+    if (gameState[itemNumber] === 'empty') {
+      gameState[itemNumber] = isCross ? 'cross' : 'circle';
+      setIsCross(!isCross);
+    } else {
+      return Snackbar.show({
+        text: 'Position is already filled',
+        backgroundColor: 'red',
+        textColor: 'white',
+      });
+    }
+
+    checkIsWinner();
   };
 
   return (
@@ -88,6 +109,8 @@ export default function App(): JSX.Element {
       <View>
         <Text>Tic Tac Toe With Rakib</Text>
       </View>
+
+      <FlatList numColumns={3} data={gameState} renderItem={item => <></>} />
     </SafeAreaView>
   );
 }
